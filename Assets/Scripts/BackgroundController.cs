30c059ba750e646708700fa967e6f3366ee5b95a
Alberto Prado <danteprado.70@gmail.com>
2020-03-29 19:24:45 -0600

    Commit inicial. Proyecto configurado y añadido fondo del escenario

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class BackgroundController : MonoBehaviour {
+
+    private Material backgroundMaterial; 
+
+    void Start() {
+    
+        backgroundMaterial = GetComponent<Renderer>().material;
+        setColor();
+
+    }
+
+    public void setColor() {
+
+        Color color1 = Random.ColorHSV(0, 1, 0.7f, 1, 0.8f, 0.9f);
+        Color color2 = Random.ColorHSV(0, 1, 0.7f, 1, 0.4f, 0.5f);
+
+        backgroundMaterial.DOColor(color1, "_Color1", 1);
+        backgroundMaterial.DOColor(color2, "_Color2", 1);
+
+
+    }
+
+}
